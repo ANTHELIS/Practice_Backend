189fb3f504514e2cec952aae4a5d2e7d59b01c44
Mithu Paul <anthelisofficial@gmail.com>
2025-12-17 04:33:28 +0530

    first commit

---
+++ P_NOTE/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+app.get("/", (req, res)=>{
+    fs.readdir("./files", (err, files)=>{
+        res.render('index', {files});
+    })
+})
+
+app.post("/create", (req, res)=>{
+    fs.writeFile(`./files/${req.body.tittle.split(' ').join('')}.txt`, req.body.content, (err)=>{
+        res.redirect("/");
+    })
+})
+
+app.get("/file/:filename", (req, res)=>{
+    fs.readFile(`./files/${req.params.filename}`, 'utf8', (err, data)=>{
+        res.render('show', {pFilename: req.params.filename, data});
+    })
+})
+
+
+app.listen(3000, ()=>{
+    console.log("server running at port 3000");
+})
